2c650763c727e9c72c2278be7ba48cc70d5e23c0
Rob Kostermans <rob.kostermans@gmail.com>
2015-07-29 00:20:27 +0200

    fadein

---
+++ KnockOut/KnockOut/components/fadein.js
@@ -0,0 +1,13 @@
+﻿
+
+$(document).on('scroll touchmove mousewheel', function (e) {
+    $("body").addClass("reveal");
+    $(document).off('scroll touchmove mousewheel')
+
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+
+});
+
+
