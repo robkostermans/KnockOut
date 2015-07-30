9e92135b8891884e5bb613cb034e621195121353
Rob Kostermans <rob.kostermans@gmail.com>
2015-07-30 23:52:50 +0200

    test

--- KnockOut/KnockOut/components/fadein.js
+++ KnockOut/KnockOut/components/fadein.js
@@ -1,13 +1,25 @@
 ﻿
-
-$(document).on('scroll touchmove mousewheel', function (e) {
-    $("body").addClass("reveal");
-    $(document).off('scroll touchmove mousewheel')
-
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-
+window.currentScrollPosition = 0;
+$(window).on('scroll touchmove ', function (e) {
+    reactToScroll(e);  
 });
 
+
+
+
+reactToScroll = function (e) {
+    delta = window.currentScrollPosition - $(this).scrollTop();
+    window.currentScrollPosition = $(this).scrollTop();
+    if (delta < 0 && !$("body").hasClass("reveal")) {
+        $("body").addClass("reveal");
+        window.scrollTo(0, 0);
+        e.preventDefault();
+        window.currentScrollPosition = delta
+    } else if (delta > 0 && $(this).scrollTop() == 0 && $("body").hasClass("reveal")) {
+        $("body").removeClass("reveal");
+        
+    }
+
+    
+}
 
